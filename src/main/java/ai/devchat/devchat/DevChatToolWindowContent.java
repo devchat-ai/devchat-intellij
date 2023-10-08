3abe45e3f8c13dfaf68db067a3ccf1801b8793f9
Daniel Hu <tao.hu@merico.dev>
2023-10-08 21:31:51 +0800

    First input box in toolWindow
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

--- src/main/java/ai/devchat/devchat/DevChatToolWindowContent.java
+++ src/main/java/ai/devchat/devchat/DevChatToolWindowContent.java
@@ -5,6 +5,8 @@ import com.intellij.ui.jcef.JBCefBrowser;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import java.net.URL;
 
 public class DevChatToolWindowContent {
 
@@ -19,11 +21,34 @@ public class DevChatToolWindowContent {
 
         JBCefBrowser jbCefBrowser = new JBCefBrowser();
         this.content.add(jbCefBrowser.getComponent(), BorderLayout.CENTER);
-        String htmlContent = "<html><body><h1>Hello World!</h1></body></html>";
+        String htmlContent = readHTMLFile("/static/main.html");
+        if (htmlContent.isEmpty()) {
+            htmlContent = "<html><body><h1>Error: main.html not found</h1></body></html>";
+        }
         jbCefBrowser.loadHTML(htmlContent);
     }
 
     public JPanel getContent() {
         return content;
+    }
+
+    private String readHTMLFile(String fileName) {
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            URL url = getClass().getResource(fileName);
+            if (url == null) {
+                System.out.println("File not found: " + fileName);
+                return null;
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                contentBuilder.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
     }
 }
