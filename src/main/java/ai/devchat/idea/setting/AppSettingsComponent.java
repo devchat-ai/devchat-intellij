b5c4f35879891a05d8be1235e18d577b38d514e9
Daniel Hu <tao.hu@merico.dev>
2023-10-25 20:08:37 +0800

    Add AppSetting implement
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

---
+++ src/main/java/ai/devchat/idea/setting/AppSettingsComponent.java
@@ -0,0 +1,42 @@
+package ai.devchat.idea.setting;
+
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBTextField;
+import com.intellij.util.ui.FormBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * Supports creating and managing a {@link JPanel} for the Settings Dialog.
+ */
+public class AppSettingsComponent {
+
+  private final JPanel myMainPanel;
+  private final JBTextField myUserNameText = new JBTextField();
+
+  public AppSettingsComponent() {
+    myMainPanel = FormBuilder.createFormBuilder()
+            .addLabeledComponent(new JBLabel("api_base"), myUserNameText, 1, false)
+            .addComponentFillVertically(new JPanel(), 0)
+            .getPanel();
+  }
+
+  public JPanel getPanel() {
+    return myMainPanel;
+  }
+
+  public JComponent getPreferredFocusedComponent() {
+    return myUserNameText;
+  }
+
+  @NotNull
+  public String getUserNameText() {
+    return myUserNameText.getText();
+  }
+
+  public void setUserNameText(@NotNull String newText) {
+    myUserNameText.setText(newText);
+  }
+
+}
---
+++ src/main/java/ai/devchat/idea/setting/AppSettingsState.java
@@ -0,0 +1,40 @@
+package ai.devchat.idea.setting;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Supports storing the application settings in a persistent way.
+ * The {@link State} and {@link Storage} annotations define the name of the data and the file name where
+ * these persistent application settings are stored.
+ */
+@State(
+        name = "org.intellij.sdk.settings.AppSettingsState",
+        storages = @Storage("DevChatSettings.xml")
+)
+public class AppSettingsState implements PersistentStateComponent<AppSettingsState> {
+
+  public String apiBase = "https://api.devchat.ai/v1";
+
+
+  public static AppSettingsState getInstance() {
+    return ApplicationManager.getApplication().getService(AppSettingsState.class);
+  }
+
+  @Nullable
+  @Override
+  public AppSettingsState getState() {
+    return this;
+  }
+
+  @Override
+  public void loadState(@NotNull AppSettingsState state) {
+    XmlSerializerUtil.copyBean(state, this);
+  }
+
+}
