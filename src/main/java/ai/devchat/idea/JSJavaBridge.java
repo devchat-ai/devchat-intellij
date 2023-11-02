134761653dbf6a1ba70294d132fcbe725dea9540
Daniel Hu <tao.hu@merico.dev>
2023-11-02 15:00:59 +0800

    Refactor callJava to use Reflection and remove switch case
    - Modified 'callJava()' method to use reflection for calling the requested action handler's functions.
    - Deleted switch statement in 'callJava()' method which handled action request based on action type.
    - Created 'actionHandlerMap', which stores relation between action type and the corresponding action handler class.
    - Added setMetadata and setPayload methods into the ActionHandler interface.

--- src/main/java/ai/devchat/idea/JSJavaBridge.java
+++ src/main/java/ai/devchat/idea/JSJavaBridge.java
@@ -1,9 +1,9 @@
 package ai.devchat.idea;
 
+import ai.devchat.common.Log;
+import ai.devchat.devchat.ActionHandler;
 import ai.devchat.devchat.DevChatActionHandler;
 import ai.devchat.devchat.DevChatActions;
-import ai.devchat.common.Log;
-
 import ai.devchat.devchat.handler.*;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -14,6 +14,11 @@ import org.cef.browser.CefBrowser;
 import org.cef.browser.CefFrame;
 import org.cef.handler.CefLoadHandlerAdapter;
 import org.cef.network.CefRequest;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JSJavaBridge {
     private final JBCefBrowser jbCefBrowser;
@@ -39,43 +44,31 @@ public class JSJavaBridge {
         JSONObject metadata = jsonObject.getJSONObject("metadata");
         JSONObject payload = jsonObject.getJSONObject("payload");
 
-        DevChatActionHandler handler = DevChatActionHandler.getInstance();
-
+        DevChatActionHandler devChatActionHandler = DevChatActionHandler.getInstance();
         Log.info("Got action: " + action);
-        switch (action) {
-            case DevChatActions.SEND_MESSAGE_REQUEST:
-                SendMessageRequestHandler sendMessageRequestHandler = new SendMessageRequestHandler(handler);
-                sendMessageRequestHandler.setMetadata(metadata);
-                sendMessageRequestHandler.setPayload(payload);
-                sendMessageRequestHandler.executeAction();
-                break;
-            case DevChatActions.SET_OR_UPDATE_KEY_REQUEST:
-                SetOrUpdateKeyRequestHandler setOrUpdateKeyRequestHandler = new SetOrUpdateKeyRequestHandler(handler);
-                setOrUpdateKeyRequestHandler.setMetadata(metadata);
-                setOrUpdateKeyRequestHandler.setPayload(payload);
-                setOrUpdateKeyRequestHandler.executeAction();
-                break;
-            case DevChatActions.LIST_COMMANDS_REQUEST:
-                ListCommandsRequestHandler listCommandsRequestHandler = new ListCommandsRequestHandler(handler);
-                listCommandsRequestHandler.setMetadata(metadata);
-                listCommandsRequestHandler.setPayload(payload);
-                listCommandsRequestHandler.executeAction();
-                break;
-            case DevChatActions.LIST_CONVERSATIONS_REQUEST:
-                ListConversationsRequestHandler listConversationsRequestHandler = new ListConversationsRequestHandler(handler);
-                listConversationsRequestHandler.setMetadata(metadata);
-                listConversationsRequestHandler.setPayload(payload);
-                listConversationsRequestHandler.executeAction();
-                break;
-            case DevChatActions.LIST_TOPICS_REQUEST:
-                ListTopicsRequestHandler listTopicsRequestHandler = new ListTopicsRequestHandler(handler);
-                listTopicsRequestHandler.setMetadata(metadata);
-                listTopicsRequestHandler.setPayload(payload);
-                listTopicsRequestHandler.executeAction();
-                break;
+
+        ActionHandler actionHandler;
+        try {
+            Constructor<?> constructor = actionHandlerMap.get(action).getConstructor(DevChatActionHandler.class);
+            actionHandler = (ActionHandler) constructor.newInstance(devChatActionHandler);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            Log.error("Failed to instantiate action handler: " + e.getMessage());
+            throw new RuntimeException(e);
         }
+        actionHandler.setMetadata(metadata);
+        actionHandler.setPayload(payload);
+        actionHandler.executeAction();
+
         return new JBCefJSQuery.Response("ignore me");
     }
+
+    private final Map<String, Class<? extends ActionHandler>> actionHandlerMap = new HashMap<>() {{
+        put(DevChatActions.SEND_MESSAGE_REQUEST, SendMessageRequestHandler.class);
+        put(DevChatActions.SET_OR_UPDATE_KEY_REQUEST, SetOrUpdateKeyRequestHandler.class);
+        put(DevChatActions.LIST_COMMANDS_REQUEST, ListCommandsRequestHandler.class);
+        put(DevChatActions.LIST_CONVERSATIONS_REQUEST, ListConversationsRequestHandler.class);
+        put(DevChatActions.LIST_TOPICS_REQUEST, ListTopicsRequestHandler.class);
+    }};
 
     public void registerToBrowser() {
         this.jbCefBrowser.getJBCefClient().addLoadHandler(new CefLoadHandlerAdapter() {
