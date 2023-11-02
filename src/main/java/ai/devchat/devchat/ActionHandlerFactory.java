00c6e852e3a15d820493762817e1806174112881
Daniel Hu <tao.hu@merico.dev>
2023-11-02 15:19:51 +0800

    Implement ActionHandlerFactory and refactor JSJavaBridge
    - Created a new class ActionHandlerFactory that encapsulates the process of creating ActionHandler instances based on the action string.
    - ActionHandlerFactory uses reflection to dynamically instantiate the appropriate ActionHandler subtype.
    - Modified JSJavaBridge to use the new ActionHandlerFactory to create ActionHandler instances.
    - Removed the actionHandlerMap from JSJavaBridge as this function is now covered by the ActionHandlerFactory.

---
+++ src/main/java/ai/devchat/devchat/ActionHandlerFactory.java
@@ -0,0 +1,36 @@
+package ai.devchat.devchat;
+
+import ai.devchat.devchat.handler.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActionHandlerFactory {
+    private final Map<String, Class<? extends ActionHandler>> actionHandlerMap = new HashMap<>() {
+        {
+            put(DevChatActions.SEND_MESSAGE_REQUEST, SendMessageRequestHandler.class);
+            put(DevChatActions.SET_OR_UPDATE_KEY_REQUEST, SetOrUpdateKeyRequestHandler.class);
+            put(DevChatActions.LIST_COMMANDS_REQUEST, ListCommandsRequestHandler.class);
+            put(DevChatActions.LIST_CONVERSATIONS_REQUEST, ListConversationsRequestHandler.class);
+            put(DevChatActions.LIST_TOPICS_REQUEST, ListTopicsRequestHandler.class);
+        }
+    };
+
+    public ActionHandler createActionHandler(String action) {
+        Class<? extends ActionHandler> handlerClass = actionHandlerMap.get(action);
+        if (handlerClass != null) {
+            try {
+                Constructor<? extends ActionHandler> constructor = handlerClass
+                        .getConstructor(DevChatActionHandler.class);
+                return constructor.newInstance(DevChatActionHandler.getInstance());
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException
+                     | InvocationTargetException e) {
+                throw new RuntimeException("Failed to instantiate action handler for: " + action, e);
+            }
+        } else {
+            throw new RuntimeException("Action handler not found: " + action);
+        }
+    }
+}
--- src/main/java/ai/devchat/idea/JSJavaBridge.java
+++ src/main/java/ai/devchat/idea/JSJavaBridge.java
@@ -2,9 +2,8 @@ package ai.devchat.idea;
 
 import ai.devchat.common.Log;
 import ai.devchat.devchat.ActionHandler;
-import ai.devchat.devchat.DevChatActionHandler;
-import ai.devchat.devchat.DevChatActions;
-import ai.devchat.devchat.handler.*;
+import ai.devchat.devchat.ActionHandlerFactory;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.intellij.ui.jcef.JBCefBrowser;
@@ -14,11 +13,6 @@ import org.cef.browser.CefBrowser;
 import org.cef.browser.CefFrame;
 import org.cef.handler.CefLoadHandlerAdapter;
 import org.cef.network.CefRequest;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JSJavaBridge {
     private final JBCefBrowser jbCefBrowser;
@@ -43,32 +37,15 @@ public class JSJavaBridge {
         String action = jsonObject.getString("action");
         JSONObject metadata = jsonObject.getJSONObject("metadata");
         JSONObject payload = jsonObject.getJSONObject("payload");
-
-        DevChatActionHandler devChatActionHandler = DevChatActionHandler.getInstance();
         Log.info("Got action: " + action);
 
-        ActionHandler actionHandler;
-        try {
-            Constructor<?> constructor = actionHandlerMap.get(action).getConstructor(DevChatActionHandler.class);
-            actionHandler = (ActionHandler) constructor.newInstance(devChatActionHandler);
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            Log.error("Failed to instantiate action handler: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
+        ActionHandler actionHandler = new ActionHandlerFactory().createActionHandler(action);
         actionHandler.setMetadata(metadata);
         actionHandler.setPayload(payload);
         actionHandler.executeAction();
 
         return new JBCefJSQuery.Response("ignore me");
     }
-
-    private final Map<String, Class<? extends ActionHandler>> actionHandlerMap = new HashMap<>() {{
-        put(DevChatActions.SEND_MESSAGE_REQUEST, SendMessageRequestHandler.class);
-        put(DevChatActions.SET_OR_UPDATE_KEY_REQUEST, SetOrUpdateKeyRequestHandler.class);
-        put(DevChatActions.LIST_COMMANDS_REQUEST, ListCommandsRequestHandler.class);
-        put(DevChatActions.LIST_CONVERSATIONS_REQUEST, ListConversationsRequestHandler.class);
-        put(DevChatActions.LIST_TOPICS_REQUEST, ListTopicsRequestHandler.class);
-    }};
 
     public void registerToBrowser() {
         this.jbCefBrowser.getJBCefClient().addLoadHandler(new CefLoadHandlerAdapter() {
@@ -78,8 +55,8 @@ public class JSJavaBridge {
                         "window.JSJavaBridge = {"
                                 + "callJava : function(arg) {"
                                 + jsQuery.inject("arg",
-                                "response => console.log(response)",
-                                "(error_code, error_message) => console.log('callJava Failed', error_code, error_message)")
+                                        "response => console.log(response)",
+                                        "(error_code, error_message) => console.log('callJava Failed', error_code, error_message)")
                                 + "}"
                                 + "};",
                         "", 0);
