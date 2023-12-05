39b884132aba769f8e28800459b0f0529b528d47
Luo Tim <shiwen@merico.dev>
2023-12-05 18:55:46 +0800

    Implement ActiveConversation to manage active conversation with cache

---
+++ src/main/kotlin/ai/devchat/idea/storage/ActiveConversation.kt
@@ -0,0 +1,41 @@
+package ai.devchat.idea.storage
+
+import com.alibaba.fastjson.JSONObject
+
+
+object ActiveConversation {
+    private var messages: MutableList<JSONObject>? = null
+    private var topic: String? = null
+
+    fun reset(topic: String, messages: MutableList<JSONObject>) {
+        this.topic = topic
+        this.messages = messages
+    }
+
+    fun addMessage(message: JSONObject) {
+        messages?.add(message)
+    }
+
+    fun findMessage(hash: String): JSONObject? {
+        return messages?.find{it.getString("hash") == hash}
+    }
+
+    fun deleteMessage(hash: String) {
+        val idx = messages?.indexOfFirst {it.getString("hash") == hash} ?: -1
+        if (idx >= 0) {
+            messages?.slice(0..<idx)
+        }
+    }
+
+    val lastMessage get() = messages?.lastOrNull()
+
+    fun getMessages(page: Int = 1, pageSize: Int = 20): List<JSONObject>? {
+        if (this.messages == null) {
+            return null
+        }
+        return this.messages!!.asReversed().slice(
+            pageSize * (page - 1)..<pageSize * page
+        ).reversed()
+    }
+}
+
