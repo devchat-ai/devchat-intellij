2b7266459c667981571925ec61fc4f9cc72d2737
Luo Tim <shiwen@merico.dev>
2023-12-11 18:32:18 +0800

    Fix settings opening

--- src/main/kotlin/ai/devchat/idea/settings/SupportedModels.kt
+++ src/main/kotlin/ai/devchat/idea/settings/SupportedModels.kt
@@ -1,9 +1,9 @@
 package ai.devchat.idea.settings
 
 val supportedModels = setOf(
-    "gpt-3.5",
+    "gpt-3.5-turbo",
 //    "gpt-3.5-turbo-1106",
-    "gpt-3.5-16k",
+    "gpt-3.5-turbo-16k",
     "gpt-4",
 //    "gpt-4-1106-preview",
     "claude-2",
