9b29002f5769f1b9ce647f73a77c9f63eff07338
Luo Tim <shiwen@merico.dev>
2024-04-17 22:45:06 +0800

    Add complete language map

--- src/main/kotlin/ai/devchat/core/handlers/NewSrcFile.kt
+++ src/main/kotlin/ai/devchat/core/handlers/NewSrcFile.kt
@@ -6,10 +6,7 @@ import ai.devchat.plugin.currentProject
 import com.alibaba.fastjson.JSONObject
 import com.intellij.lang.Language
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.command.CommandProcessor
-import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiManager
@@ -26,10 +23,12 @@ class NewSrcFile(requestAction: String, metadata: JSONObject?, payload: JSONObje
         ApplicationManager.getApplication().invokeLater {
             val project = currentProject ?: return@invokeLater
             val dir = FileEditorManager.getInstance(project).selectedEditor?.file?.parent ?: return@invokeLater
-            WriteCommandAction.runWriteCommandAction(project) {
-                val psiDirectory = PsiManager.getInstance(project).findDirectory(dir) ?: return@runWriteCommandAction
-                val fileLanguage = getLanguageByName(language) ?: return@runWriteCommandAction
-                val psiFile = PsiFileFactory.getInstance(project).createFileFromText(fileLanguage, content)
+            ApplicationManager.getApplication().runWriteAction {
+                val psiDirectory = PsiManager.getInstance(project).findDirectory(dir) ?: return@runWriteAction
+                val (fileLanguage, ext) = getLanguageByName(language) ?: return@runWriteAction
+                val psiFile = PsiFileFactory.getInstance(project).createFileFromText(
+                    "foo$ext", fileLanguage, content
+                )
                 val addedFile = (psiDirectory.add(psiFile) as PsiFile).virtualFile
                 addedFile.refresh(false, false)
                 FileEditorManager.getInstance(project).openFile(addedFile, true)
@@ -38,15 +37,65 @@ class NewSrcFile(requestAction: String, metadata: JSONObject?, payload: JSONObje
         send()
     }
 
-    private fun getLanguageByName(markdownName: String): Language? {
+    private fun getLanguageByName(markdownName: String): Pair<Language, String>? {
         val markdownToLanguageId = mapOf(
-            "java" to "JAVA",
-            "kotlin" to "kotlin",
-            "python" to "Python",
-            "js" to "JavaScript",
+            // IDEA
+            "toml" to Pair("TOML", ".toml"),
+            "html" to Pair("HTML", ".html"),
+            "json" to Pair("JSON", ".json"),
+            "xml" to Pair("XML", ".xml"),
+            "yaml" to Pair("yaml", ".yaml"),
+            "yml" to Pair("yaml", ".yml"),
+            "markdown" to Pair("Markdown", ".md"),
+            "shell" to Pair("Shell Script", ".sh"),
+            "sh" to Pair("Shell Script", ".sh"),
+            "java" to Pair("JAVA", ".java"),
+            "groovy" to Pair("Groovy", ".groovy"),
+            "kotlin" to Pair("kotlin", ".kt"),
+
+            // Webstorm
+            "js" to Pair("JavaScript", ".js"),
+            "javascript" to Pair("JavaScript", ".js"),
+            "css" to Pair("CSS", ".css"),
+            "less" to Pair("LESS", ".less"),
+            "vue" to Pair("Vue", ".vue"),
+            "ts" to Pair("TypeScript", ".ts"),
+            "typescript" to Pair("TypeScript", ".ts"),
+            "dockerfile" to Pair("Dockerfile", ".dockerfile"),
+
+            // GoLand
+            "makefile" to Pair("Makefile", ""),
+            "golang" to Pair("go", ".go"),
+            "go" to Pair("go", ".go"),
+
+            // PyCharm
+            "python" to Pair("Python", ".py"),
+
+            // RubyMine
+            "ruby" to Pair("ruby", ".rb"),
+            "rb" to Pair("ruby", ".rb"),
+            "tsx" to Pair("TypeScript JSX", ".tsx"),
+            "sass" to Pair("SASS", ".sass"),
+            "sql" to Pair("SQL", ".sql"),
+            "scss" to Pair("SCSS", ".scss"),
+            "coffeescript" to Pair("CoffeeScript", ".coffee"),
+
+            // PHPStorm
+            "php" to Pair("PHP", ".php"),
+
+            // RustRover
+            "rs" to Pair("Rust", ".rs"),
+            "rust" to Pair("Rust", ".rs"),
+
+            "scala" to Pair("Scala", ".sc"),
+            "cpp" to Pair("CPP", ".cpp"),
+            "c" to Pair("C", ".c"),
+            "objectivec" to Pair("ObjectiveC", ".m"),
         )
-        val languageId = markdownToLanguageId[markdownName.lowercase()] ?: return null
-        return Language.findLanguageByID(languageId)
+        val (languageId, ext) = markdownToLanguageId[markdownName.lowercase()] ?: return null
+        return Language.findLanguageByID(languageId)?.let {
+            Pair(it, ext)
+        }
     }
 
 }
