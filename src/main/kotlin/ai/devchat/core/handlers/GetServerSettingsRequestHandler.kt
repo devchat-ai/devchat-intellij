5ffffc29ae9633448fae31ca05fd6e29361acf0d
Luo Tim <shiwen@merico.dev>
2024-06-15 23:18:10 +0800

    Remove unused code

--- src/main/kotlin/ai/devchat/core/handlers/GetServerSettingsRequestHandler.kt
+++ src/main/kotlin/ai/devchat/core/handlers/GetServerSettingsRequestHandler.kt
@@ -4,7 +4,6 @@ import ai.devchat.core.BaseActionHandler
 import ai.devchat.core.DevChatActions
 import ai.devchat.storage.SERVER_CONFIG
 import com.alibaba.fastjson.JSONObject
-import com.intellij.openapi.diagnostic.Logger
 
 
 class GetServerSettingsRequestHandler(requestAction: String, metadata: JSONObject?, payload: JSONObject?) : BaseActionHandler(
@@ -12,11 +11,9 @@ class GetServerSettingsRequestHandler(requestAction: String, metadata: JSONObjec
     metadata,
     payload
 ) {
-    private val logger = Logger.getInstance(GetServerSettingsRequestHandler::class.java)
     override val actionName: String = DevChatActions.GET_SERVER_SETTINGS_RESPONSE
     @Suppress("UNCHECKED_CAST")
     override fun action() {
-        logger.info("^^^^^^^^^^^^^^^^^^^^^ get server settings")
         send(payload= SERVER_CONFIG.get() as? Map<String, *>)
     }
 }
